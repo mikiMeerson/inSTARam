2f19b10b0c776d97f7a17d91baf8a007a847ede2
mikim <mikimeerson44@gmail.com>
2022-01-23 20:06:09 +0200

    design activity log

---
+++ src/Layouts/stars/feed/activityItem.tsx
@@ -0,0 +1,56 @@
+import { Typography } from '@mui/material';
+import {
+  StarBorderOutlined,
+  CheckCircleOutlined,
+  TextSnippetOutlined,
+  OfflineBoltOutlined,
+} from '@mui/icons-material';
+
+import { activityLogType } from '../../../assets/star';
+
+interface activityProps {
+    log: activityLogType;
+}
+
+const actionToIcon = [
+  {
+    action: 'יצר/ה את הסטאר',
+    icon: <StarBorderOutlined className="logIcon" />,
+  },
+  {
+    action: 'שינת/ה את הסטטוס',
+    icon: <CheckCircleOutlined className="logIcon" />,
+  },
+  {
+    action: 'הוסיפ/ה הערה חדשה',
+    icon: <TextSnippetOutlined className="logIcon" />,
+  },
+  {
+    action: 'הוסיפ/ה משאבים נדרשים',
+    icon: <OfflineBoltOutlined className="logIcon" />,
+  },
+];
+
+const ActivityItem = ({ log }: activityProps) => (
+  <div className="activityItem">
+    {actionToIcon.filter((a) => a.action === log.action).map((e) => e.icon)}
+    <Typography
+      sx={{ fontSize: 14 }}
+      color="text.secondary"
+      gutterBottom
+    >
+      <span className="logPublisher">{log.publisher}</span>
+      <span>
+        {' '}
+        {log.action}
+      </span>
+      <span style={{ display: log.value ? '' : 'none' }}>
+        {' '}
+        ל
+        <span className="logValue">{log.value}</span>
+      </span>
+    </Typography>
+  </div>
+);
+
+export default ActivityItem;
--- src/Layouts/stars/feed/starActivity.tsx
+++ src/Layouts/stars/feed/starActivity.tsx
@@ -1,5 +1,6 @@
-import { Typography, Card, CardContent } from '@mui/material';
+import { Typography } from '@mui/material';
 import { activityLogType } from '../../../assets/star';
+import ActivityItem from './activityItem';
 
 interface activityProps {
   activity: activityLogType[];
@@ -15,40 +16,36 @@ const StarActivity = ({ activity }: activityProps) => {
       </div>
     );
   }
+
+  const activityDates = activity
+    .map((log) => log.time).map((date) => date.getTime())
+    .filter((date, i, array) => array.indexOf(date) === i)
+    .map((time) => new Date(time));
+
   return (
     <div className="feedSection">
       <Typography variant="h5" paddingBottom="10px">
         פעילות
       </Typography>
-      {activity.map((log: activityLogType) => (
-        <Card
-          sx={{
-            height: '50px',
-            margin: '10px',
-            border: '1px solid silver',
-            borderRadius: '2px',
-          }}
-        >
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              <span>{log.publisher}</span>
-              <span>
-                {' '}
-                {log.action}
-              </span>
-              <span style={{ display: log.value ? '' : 'none' }}>
-                {' '}
-                ל-
-                {log.value}
-              </span>
+      <div style={{ height: '80%', overflowY: 'scroll' }}>
+        {activityDates.map((currDate) => (
+          <div style={{ textAlign: 'right' }}>
+            <Typography variant="caption">
+              {`${currDate.getFullYear()} 
+              ${currDate.getDate()} 
+              ${currDate.toLocaleString('default', { month: 'long' })}`}
             </Typography>
-          </CardContent>
-        </Card>
-      ))}
+            {
+              activity.filter(
+                (log) => log.time.getTime() === currDate.getTime(),
+              )
+                .map((log: activityLogType) => (
+                  <ActivityItem log={log} />
+                ))
+            }
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
