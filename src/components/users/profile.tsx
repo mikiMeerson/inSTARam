da2bb275ff5d1a33fedb680255110d03a744dc34
mikim <mikimeerson44@gmail.com>
2022-02-22 22:45:06 +0200

    design profile header

--- src/components/users/profile.tsx
+++ src/components/users/profile.tsx
@@ -1,5 +1,49 @@
+import {
+  AccountCircle,
+  VisibilityOutlined,
+  AssignmentOutlined,
+} from '@mui/icons-material';
+import { Avatar, Typography, Button } from '@mui/material';
+import './styles/profile.css';
+
 const Profile = () => (
-  <div>User profile</div>
+  <>
+    <div className="profileHeader">
+      <Avatar sx={{ width: '150px', height: '150px' }}>
+        <AccountCircle sx={{ width: '150px', height: '150px' }} />
+      </Avatar>
+      <div className="userInfo">
+        <Typography variant="h4">
+          {localStorage.getItem('userDisplay')}
+        </Typography>
+        <div className="userDetails">
+          <Button variant="outlined" size="large" color="warning">
+            <VisibilityOutlined />
+            <span className="buttonText">דפים שאתה צופה בהם</span>
+          </Button>
+          <Button variant="outlined" size="large" color="info">
+            <AssignmentOutlined />
+            <span className="buttonText">הפעילות שלך</span>
+          </Button>
+        </div>
+        <div>
+          <Typography variant="subtitle1">
+            לשינוי הפרטים האישיים
+            {' '}
+            <Button variant="text" color="secondary">
+              לחץ כאן
+            </Button>
+          </Typography>
+        </div>
+      </div>
+    </div>
+    <div>
+      הפעילות שלך
+    </div>
+    <div>
+      דפים שאתה צופה בהם
+    </div>
+  </>
 );
 
 export default Profile;
