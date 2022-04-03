b33b7d037a71f6928e6d8e4e66143817403c1e8d
mikim <mikimeerson44@gmail.com>
2022-04-03 19:04:25 +0300

    separate EventsMain from all display and filter logic

---
+++ src/components/events/eventsPage/eventsList.tsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from 'react';
+import { Grid } from '@mui/material';
+import { StatusCodes } from 'http-status-codes';
+import DialogAlert from '../../general/dialogAlert';
+import FilterHeaders from '../filters/filterHeaders';
+import SearchBar from '../filters/searchBar';
+import EventCard from './eventCard';
+import { FilterDataType } from '../../../types/configurations';
+import { IEvent } from '../../../types/interfaces';
+import { getStars } from '../../../services/star-service';
+import { deleteEvent } from '../../../services/event-service';
+import { UserRole } from '../../../types/string-types';
+
+interface Props {
+    events: IEvent[];
+    setEvents: (events: IEvent[]) => void;
+    userRole: UserRole;
+    handleAlert: (isSuccess: boolean, content: string) => void;
+}
+
+const EventsList = ({ events, setEvents, userRole, handleAlert }: Props) => {
+  const getExistingFilters = (filterName: string) => {
+    const existingFilter = localStorage.getItem(`events ${filterName} `);
+    return existingFilter ? JSON.parse(existingFilter) : [];
+  };
+
+  const [filteredEvents, setFilteredEvents] = useState<IEvent[]>([]);
+  const [deleteError, setDeleteError] = useState<boolean>(false);
+  const [nameSearch, setNameSearch] = useState<string>('');
+  const [platformFilter, setPlatformFilter] = useState<string[]>(
+    getExistingFilters('platform'),
+  );
+  const [blockFilter, setBlockFilter] = useState<string[]>(
+    getExistingFilters('block'),
+  );
+  const [assigneeFilter, setAssigneeFilter] = useState<string>('');
+  const [dateFilter, setDateFilter] = useState<string[]>(
+    getExistingFilters('date'),
+  );
+
+  useEffect(() => {
+    const tempFilteredEvents: IEvent[] = [];
+    events.forEach((e) => {
+      if ((nameSearch === '' || e.name.includes(nameSearch))
+        && (platformFilter.length === 0 || platformFilter.includes(e.platform))
+        && (blockFilter.length === 0 || blockFilter.includes(e.block))
+        && (assigneeFilter === ''
+          || (e.assignee && e.assignee.includes(assigneeFilter)))) {
+        tempFilteredEvents.push(e);
+      }
+    });
+    setFilteredEvents(tempFilteredEvents);
+  }, [events, assigneeFilter, blockFilter, nameSearch, platformFilter]);
+
+  const filtersData: FilterDataType[] = [
+    {
+      tabName: 'platform',
+      filter: platformFilter,
+      func: setPlatformFilter,
+      chipColor: 'primary',
+    },
+    {
+      tabName: 'block',
+      filter: blockFilter,
+      func: setBlockFilter,
+      chipColor: 'secondary',
+    },
+    {
+      tabName: 'date',
+      filter: dateFilter,
+      func: setDateFilter,
+      chipColor: 'warning',
+    },
+  ];
+
+  const checkAttachedStars = async (event: IEvent) => {
+    const { status, data } = await getStars();
+    if (status !== StatusCodes.OK) return true; // !should be another error
+    const attachedStars = data.stars.filter((s) => s.event === event._id);
+    return attachedStars.length;
+  };
+
+  const handleDeleteEvent = async (event: IEvent) => {
+    if (await checkAttachedStars(event)) {
+      setDeleteError(true);
+    } else {
+      const { status, data } = await deleteEvent(event._id);
+      if (status !== StatusCodes.OK) {
+        handleAlert(false, 'לא הצלחנו למחוק את האירוע');
+      } else {
+        handleAlert(true, 'האירוע נמחק בהצלחה');
+        setEvents(data.events);
+      }
+    }
+  };
+
+  const sortByDate = (events: IEvent[]) => events
+    .sort((a: IEvent, b: IEvent) => {
+      if (a.dates[1] < b.dates[1]) return 1;
+      if (a.dates[1] > b.dates[1]) return -1;
+      if (a.dates[0] < b.dates[0]) return 1;
+      if (a.dates[0] > b.dates[0]) return -1;
+      return 0;
+    });
+
+  return (
+    <>
+      <FilterHeaders
+        filtersData={filtersData}
+        assigneeFilter={assigneeFilter}
+        setAssigneeFilter={setAssigneeFilter}
+      />
+      <SearchBar events={events} setSearch={setNameSearch} />
+      <Grid container className="eventsList">
+        {sortByDate(filteredEvents).map((e) => (
+          <Grid key={e._id} className="cardContainer" item xs={3}>
+            <EventCard
+              event={e}
+              handleDeleteEvent={handleDeleteEvent}
+              userRole={userRole}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <DialogAlert
+        header="שגיאה!"
+        content="לא ניתן למחוק אירוע כאשר משויכים אליו סטארים"
+        isOpen={deleteError}
+        setIsOpen={setDeleteError}
+        activateResponse={undefined}
+      />
+    </>
+  );
+};
+
+export default EventsList;
--- src/components/events/eventsPage/eventsMain.tsx
+++ src/components/events/eventsPage/eventsMain.tsx
@@ -1,25 +1,18 @@
 import { useEffect, useState } from 'react';
-import { StatusCodes } from 'http-status-codes';
 import { Link } from 'react-router-dom';
 import {
   Box,
   CircularProgress,
-  Grid,
   SpeedDial,
   Typography,
   SpeedDialIcon,
 } from '@mui/material';
 import { FlightTakeoffOutlined } from '@mui/icons-material';
-import { deleteEvent, getEvents } from '../../../services/event-service';
-import EventCard from './eventCard';
+import { getEvents } from '../../../services/event-service';
 import '../styles/event.css';
 import { IEvent } from '../../../types/interfaces';
 import { UserRole } from '../../../types/string-types';
-import DialogAlert from '../../general/dialogAlert';
-import { getStars } from '../../../services/star-service';
-import SearchBar from '../filters/searchBar';
-import FilterHeaders from '../filters/filterHeaders';
-import { FilterDataType } from '../../../types/configurations';
+import EventsList from './eventsList';
 
 interface Props {
   userRole: UserRole;
@@ -27,77 +20,8 @@ interface Props {
 }
 
 const EventsMain = ({ userRole, handleAlert }: Props) => {
-  const getExistingFilters = (filterName: string) => {
-    const existingFilter = localStorage.getItem(`events ${filterName} `);
-    return existingFilter ? JSON.parse(existingFilter) : [];
-  };
-
   const [loading, setLoading] = useState<boolean>(false);
   const [events, setEvents] = useState<IEvent[]>([]);
-  const [filteredEvents, setFilteredEvents] = useState<IEvent[]>([]);
-  const [deleteError, setDeleteError] = useState<boolean>(false);
-  const [nameSearch, setNameSearch] = useState<string>('');
-  const [platformFilter, setPlatformFilter] = useState<string[]>(
-    getExistingFilters('platform'),
-  );
-  const [blockFilter, setBlockFilter] = useState<string[]>(
-    getExistingFilters('block'),
-  );
-  const [assigneeFilter, setAssigneeFilter] = useState<string>('');
-  const [dateFilter, setDateFilter] = useState<string[]>(
-    getExistingFilters('date'),
-  );
-
-  const filtersData: FilterDataType[] = [
-    {
-      tabName: 'platform',
-      filter: platformFilter,
-      func: setPlatformFilter,
-      chipColor: 'primary',
-    },
-    {
-      tabName: 'block',
-      filter: blockFilter,
-      func: setBlockFilter,
-      chipColor: 'secondary',
-    },
-    {
-      tabName: 'date',
-      filter: dateFilter,
-      func: setDateFilter,
-      chipColor: 'warning',
-    },
-  ];
-
-  const checkAttachedStars = async (event: IEvent) => {
-    const { status, data } = await getStars();
-    if (status !== StatusCodes.OK) return true; // !should be another error
-    const attachedStars = data.stars.filter((s) => s.event === event._id);
-    return attachedStars.length;
-  };
-
-  const handleDeleteEvent = async (event: IEvent) => {
-    if (await checkAttachedStars(event)) {
-      setDeleteError(true);
-    } else {
-      const { status, data } = await deleteEvent(event._id);
-      if (status !== StatusCodes.OK) {
-        handleAlert(false, 'לא הצלחנו למחוק את האירוע');
-      } else {
-        handleAlert(true, 'האירוע נמחק בהצלחה');
-        setEvents(data.events);
-      }
-    }
-  };
-
-  const sortByDate = (events: IEvent[]) => events
-    .sort((a: IEvent, b: IEvent) => {
-      if (a.dates[1] < b.dates[1]) return 1;
-      if (a.dates[1] > b.dates[1]) return -1;
-      if (a.dates[0] < b.dates[0]) return 1;
-      if (a.dates[0] > b.dates[0]) return -1;
-      return 0;
-    });
 
   useEffect(() => {
     const fetchEvents = async (): Promise<void> => {
@@ -106,27 +30,8 @@ const EventsMain = ({ userRole, handleAlert }: Props) => {
       setEvents(data.events);
       setLoading(false);
     };
-    if (events.length === 0) fetchEvents();
-
-    const tempFilteredEvents: IEvent[] = [];
-    events.forEach((e) => {
-      if ((nameSearch === '' || e.name.includes(nameSearch))
-        && (platformFilter.length === 0 || platformFilter.includes(e.platform))
-        && (blockFilter.length === 0 || blockFilter.includes(e.block))
-        && (assigneeFilter === ''
-          || (e.assignee && e.assignee.includes(assigneeFilter)))) {
-        tempFilteredEvents.push(e);
-      }
-    });
-    setFilteredEvents(tempFilteredEvents);
-  }, [
-    assigneeFilter,
-    blockFilter,
-    dateFilter,
-    events,
-    nameSearch,
-    platformFilter,
-  ]);
+    fetchEvents();
+  }, []);
 
   return (
     <div className="events">
@@ -151,29 +56,11 @@ const EventsMain = ({ userRole, handleAlert }: Props) => {
         </Link>
         )}
       </div>
-      <FilterHeaders
-        filtersData={filtersData}
-        assigneeFilter={assigneeFilter}
-        setAssigneeFilter={setAssigneeFilter}
-      />
-      <SearchBar events={events} setSearch={setNameSearch} />
-      <Grid container className="eventsList">
-        {sortByDate(filteredEvents).map((e) => (
-          <Grid key={e._id} className="cardContainer" item xs={3}>
-            <EventCard
-              event={e}
-              handleDeleteEvent={handleDeleteEvent}
-              userRole={userRole}
-            />
-          </Grid>
-        ))}
-      </Grid>
-      <DialogAlert
-        header="שגיאה!"
-        content="לא ניתן למחוק אירוע כאשר משויכים אליו סטארים"
-        isOpen={deleteError}
-        setIsOpen={setDeleteError}
-        activateResponse={undefined}
+      <EventsList
+        events={events}
+        setEvents={setEvents}
+        userRole={userRole}
+        handleAlert={handleAlert}
       />
     </div>
   );
